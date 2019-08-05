db48c5a01e85dceb1119b124cdad78c1c4903f39
Marvin Nikijuluw <marvkipi@gmail.com>
2019-08-04 20:58:11 -0500

    creating data model

---
+++ todoey/Item.swift
@@ -0,0 +1,15 @@
+//
+//  Item.swift
+//  todoey
+//
+//  Created by Marvin Nikijuluw on 8/4/19.
+//  Copyright © 2019 Marvin Nikijuluw. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done: Bool = false 
+    
+}
